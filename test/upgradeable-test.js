ee1e048c03494536af2607f834f0f968487ddfc9
Timothy Messer <tmesser@verus.io>
2018-05-06 16:54:26 -0500

    Test for proxied methods working

--- test/upgradeable-test.js
+++ test/upgradeable-test.js
@@ -9,4 +9,12 @@ contract('Upgradeable', async (accounts) => {
     let version = await master.currentVersion.call()
     assert(version === library.address, 'Version is set to latest')
   })
+  it('should allow proxied methods', async ()=> {
+    let master = await Master.deployed()
+    let library = await Library.deployed()
+    await master.setVersion(library.address)
+    let proxy = await Library.at(master.address)
+    let foo = await proxy.foo.call()
+    assert(foo === "bar", 'Unable to call foo on upgradable contract')
+  })
 })
