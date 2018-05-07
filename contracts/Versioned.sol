089d0f6438107dc6807b8712e83ed5beb5895143
Zach Kelling <z@zeekay.io>
2018-05-07 09:57:42 -0500

    Only support single owner

--- contracts/Versioned.sol
+++ contracts/Versioned.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.4.23;
 
-import "./Administered.sol";
+import "./Owned.sol";
 import "./Proxy.sol";
 
 
-contract Versioned is Administered, Proxy {
+contract Versioned is Owned, Proxy {
     event Upgrade(address indexed predecessor, address indexed successor);
 
     // Update current version and store method configurations
-    function setVersion(address successor) public onlyAdmins {
+    function setVersion(address successor) public onlyOwner {
         address predecessor = delegate;
         delegate = successor;
         emit Upgrade(predecessor, successor);
